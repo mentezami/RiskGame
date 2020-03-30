1aec85b779428516c1f9307bf6f54a4a5df5730a
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2020-03-30 02:04:11 -0400

    add config package, Commands class and define the main commands to it

---
+++ src/main/java/config/Commands.java
@@ -0,0 +1,29 @@
+package config;
+
+public class Commands {
+
+        public static final String MAP_COMMAND_EDIT_CONTINENT = "editcontinent";
+        public static final String MAP_COMMAND_EDIT_COUNTRY = "editcountry";
+        public static final String MAP_COMMAND_EDIT_NEIGHBOR = "editneighbor";
+
+        public static final String MAP_COMMAND_SHOWMAP = "showmap";
+        public static final String MAP_COMMAND_SAVEMAP = "savemap";
+        public static final String MAP_COMMAND_EDITMAP = "editmap";
+        public static final String MAP_COMMAND_VALIDATEMAP = "validatemap";
+        public static final String MAP_COMMAND_LOADMAP = "loadmap";
+
+        public static final String MAP_COMMAND_GAMEPLAYER = "gameplayer";
+        public static final String MAP_COMMAND_POPULATE_COUNTRIES = "populatecountries";
+
+        public static final String MAP_COMMAND_PLACE_ARMY = "placearmy";
+        public static final String MAP_COMMAND_PLACE_ALL = "placeall";
+
+        public static final String MAP_COMMAND_REINFORCE = "reinforce";
+        public static final String MAP_COMMAND_FORTIFY = "fortify";
+
+        public static final String MAP_COMMAND_FORTIFY_OPTION_NONE = "none";
+
+        public static final String MAP_COMMAND_OPTION_ADD = "-add";
+        public static final String MAP_COMMAND_OPTION_REMOVE = "-remove";
+    }
+
