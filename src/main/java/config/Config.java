bf3598323e4fcae32db2a157cb6745b7ecb47fb5
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2020-03-30 02:07:32 -0400

    add Config class and set default config inside it

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,18 @@
+package config;
+
+public class Config {
+
+        public static String message = "";
+
+        public static final int CONFIG_ARMIES_TWO_PLAYER = 40;
+
+        public static final int CONFIG_ARMIES_THREE_PLAYER = 35;
+
+        public static final int CONFIG_ARMIES_FOUR_PLAYER = 30;
+
+        public static final int CONFIG_ARMIES_FIVE_PLAYER = 25;
+
+        public static final int CONFIG_ARMIES_SIX_PLAYER = 20;
+    }
+
+
