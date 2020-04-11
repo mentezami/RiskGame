20a6b3e00651755464f11e951cfa8b3e8fcf42a7
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2020-04-10 22:51:34 -0400

    add MapParserTestSuite class, PlayerParserTestSuite class and test suite methods inside them.

---
+++ src/test/java/playerparser/PlayerParserTestSuite.java
@@ -0,0 +1,16 @@
+package playerparser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * This is a Test suite Class for testing PlayerParser
+ *
+ * @author Mahmoudreza
+ * @version 0.0.1
+ */
+@RunWith(Suite.class)
+@SuiteClasses({PlayerCommandsTest.class})
+public class PlayerParserTestSuite {
+}
