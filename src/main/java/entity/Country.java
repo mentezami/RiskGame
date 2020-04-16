31caa6b0f14e481a473bc56096deac897802d66a
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2020-04-15 23:27:53 -0400

    Add some changes and JavaDoc for Continent Class, Country Class, and Hmap Class.

--- src/main/java/entity/Country.java
+++ src/main/java/entity/Country.java
@@ -3,44 +3,79 @@ package entity;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class defines properties of country like its coordinates,
+ * the continent to which country belongs, its adjacent countries,
+ * and whether it's been assigned to any player or not.
+ *
+ * @see Continent
+ * @author Mahmoudreza
+ * @version 0.0.1
+ */
 public class Country {
 
     private String name;
-
     private int xCoordinate;
     private int yCoordinate;
-
     private Player player;
     private Continent belongToContinent;
-
     private List <String> neighborCountries;
     private List <Country> adjacentCountries;
-
     private int army;
     private boolean isVisited;
 
+    /**
+     * Default constructor for the country.
+     *
+     */
     public Country() {
         neighborCountries = new ArrayList<String>();
         adjacentCountries = new ArrayList<Country>();
     }
 
-    public Country(List <String> adjCountries, List <Country> adjacentCountries) {
-        this.neighborCountries = adjCountries;
+    /**
+     * Parameterized constructor for the country.
+     *
+     * @param neighborCountries name of adjacent countries
+     * @param adjacentCountries object of adjacent countries
+     */
+    public Country(List <String> neighborCountries, List <Country> adjacentCountries) {
+        this.neighborCountries = neighborCountries;
         this.adjacentCountries = adjacentCountries;
     }
 
-    public int getArmy() {
-        return army;
-    }
-
+    /**
+     * Getter method for the player.
+     *
+     * @return the player
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Setter method for the player.
+     *
+     * @param player
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
 
+    /**
+     * Getter method for the country armies.
+     *
+     * @return army
+     */
+    public int getArmy() {
+        return army;
+    }
+
+    /**
+     * Setter method for the country armies count.
+     *
+     * @param army
+     */
     public void setArmy(int army) {
         if (army > 5000) {
             this.army = 5000;
@@ -49,67 +84,144 @@ public class Country {
         this.army = Math.abs(army);
     }
 
+    /**
+     * Getter method for the country name.
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter method for the country name.
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter method for the X coordinate of the country.
+     *
+     * @return xCoordinate
+     */
     public int getxCoordinate() {
         return xCoordinate;
     }
 
+
+    /**
+     * Setter method for the X coordinate of the country.
+     *
+     * @param xCoordinate
+     */
     public void setxCoordinate(int xCoordinate) {
         this.xCoordinate = xCoordinate;
     }
 
+    /**
+     * Getter method for the y coordinate of the country.
+     *
+     * @return yCoordinate
+     */
     public int getyCoordinate() {
         return yCoordinate;
     }
 
+    /**
+     * Setter method for the y coordinate of the country.
+     *
+     * @param yCoordinate set yCoordinate
+     */
     public void setyCoordinate(int yCoordinate) {
         this.yCoordinate = yCoordinate;
     }
 
+    /**
+     * Getter method to returns continent which belongs to the country.
+     *
+     * @return belongToContinent
+     */
     public Continent getBelongToContinent() {
         return belongToContinent;
     }
 
+    /**
+     * Setter method to set the continent for the country.
+     *
+     * @param belongToContinent
+     */
     public void setBelongToContinent(Continent belongToContinent) {
         this.belongToContinent = belongToContinent;
     }
 
+    /**
+     * Getter method to return the adjacent countries of country.
+     *
+     * @return neighborCountries
+     */
     public List<String> getNeighborCountries() {
         return neighborCountries;
     }
 
-    public void setNeighborCountries(List <String> adjCountries) {
-        this.neighborCountries = adjCountries;
+    /**
+     * Setter method for the adjacent countries to the country.
+     *
+     * @param neighborCountries
+     */
+    public void setNeighborCountries(List <String> neighborCountries) {
+        this.neighborCountries = neighborCountries;
     }
 
+    /**
+     * Getter method to return the adjacent countries to the countries.
+     *
+     * @return adjacentCountries
+     */
     public List<Country> getAdjacentCountries() {
         return adjacentCountries;
     }
 
+    /**
+     * Setter method for the adjacent countries to the countries.
+     *
+     * @param adjacentCountries
+     */
     public void setAdjacentCountries(List <Country> adjacentCountries) {
         this.adjacentCountries = adjacentCountries;
     }
 
+    /**
+     * Checks whether the country is processed or not.
+     *
+     * @return boolean value whether a country is processed or not
+     */
     public boolean isVisited() {
         return isVisited;
     }
 
+    /**
+     * Setter method for the country as processed.
+     *
+     * @param isProcessed
+     */
     public void setVisited(boolean isProcessed) {
         this.isVisited = isProcessed;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object input_obj) {
 
@@ -120,7 +232,6 @@ public class Country {
             return true;
         }
         Country t = (Country) input_obj;
-
         return t.getName().equalsIgnoreCase(name);
     }
 }
--- src/main/java/entity/Hmap.java
+++ src/main/java/entity/Hmap.java
@@ -3,6 +3,14 @@ package entity;
 import java.io.*;
 import java.util.*;
 
+/**
+ * This class describes Map and its list of continents.
+ *
+ * @see Country
+ * @see Continent
+ * @author Mahmoudreza
+ * @version 0.0.1
+ */
 public class Hmap {
 
     private List<Continent> continents;
@@ -10,6 +18,10 @@ public class Hmap {
     private HashMap<String, Integer> countriesIdxMap;
     private HashMap<String, String> filesAttribute;
 
+    /**
+     * This is the default constructor of Hmap class.
+     *
+     */
     public Hmap() {
         filesAttribute = new HashMap <String, String>();
         continents = new ArrayList <Continent>();
@@ -17,6 +29,11 @@ public class Hmap {
         countriesIdxMap = new HashMap <String,Integer>();
     }
 
+    /**
+     * This is parameterized constructor for map.
+     *
+     * @param newMap The new map object.
+     */
     public Hmap(Hmap newMap) {
         filesAttribute = new HashMap <String, String>(newMap.filesAttribute);
         continents = new ArrayList <Continent>(newMap.continents);
@@ -24,45 +41,95 @@ public class Hmap {
         countriesIdxMap = new HashMap <String,Integer>(newMap.countriesIdxMap);
     }
 
+    /**
+     * Returns the map data.
+     *
+     * @return filesAttribute
+     */
     public HashMap <String, String> getMapData() {
         return filesAttribute;
     }
 
+    /**
+     * This sets the map data.
+     *
+     * @param mapData
+     */
     public void setMapData(HashMap <String, String> mapData) {
-        this.filesAttribute = mapData;
+        filesAttribute = mapData;
     }
 
+    /**
+     * It returns list of continents.
+     *
+     * @return continents
+     */
     public List <Continent> getContinents() {
         return continents;
     }
 
+    /**
+     * This sets the continents.
+     *
+     * @param continents
+     */
     public void setContinents(List <Continent> continents) {
         this.continents = continents;
     }
 
+    /**
+     * Returns the continent index map.
+     *
+     * @return countriesIdxMap
+     */
     public HashMap <String, Integer> getCountriesIdxMap() {
         return countriesIdxMap;
     }
 
+    /**
+     * This sets the continent index map.
+     *
+     * @param countriesIdxMap
+     */
     public void setCountriesIdxMap(HashMap <String, Integer> countriesIdxMap) {
         this.countriesIdxMap = countriesIdxMap;
     }
 
+    /**
+     * Returns the continent maps.
+     *
+     * @return continentMap
+     */
     public HashMap <String, Continent> getContinentMap() {
         return continentMap;
     }
 
+    /**
+     * This sets the continent map.
+     *
+     * @param continentMap
+     */
     public void setContinentMap(HashMap <String, Continent> continentMap) {
         this.continentMap = continentMap;
     }
 
+    /**
+     * The update change method is used for observers.
+     *
+     */
     public void setChangedForMap() {
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     public String toString() {
         return "Hmap [ mapData = " + filesAttribute + ", continents = " + continents + ", continentMap = " + continentMap + "]";
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#clone()
+     */
     public Object clone() throws CloneNotSupportedException {
         ObjectOutputStream output_obj = null;
         ObjectInputStream input_obj = null;
@@ -70,6 +137,8 @@ public class Hmap {
         try {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             output_obj = new ObjectOutputStream(bos);
+
+            // serialize and pass the object
             output_obj.writeObject(this);
             output_obj.flush();
             ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
@@ -82,7 +151,7 @@ public class Hmap {
                 output_obj.close();
                 input_obj.close();
             } catch (IOException e) {
-
+                // Auto-generated catch block
                 System.out.println("Exception: " + e.toString());
             }
         }
