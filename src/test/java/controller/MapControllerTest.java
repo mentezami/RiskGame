3d2ce4b36e3f80d30c78599f9ab2794b5c45f779
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2020-05-24 20:42:56 -0400

    Add ControllerTestSuite class and few updates in MapControllerTest class.

--- src/test/java/controller/MapControllerTest.java
+++ src/test/java/controller/MapControllerTest.java
@@ -21,7 +21,6 @@ import exception.InvalidMap;
  */
 public class MapControllerTest {
 
-    MapController mapController;
     static Continent continent;
     static Country firstCountry;
     static Country secondCountry;
@@ -47,7 +46,6 @@ public class MapControllerTest {
      */
     @Before
     public void beforeEachTest() {
-        mapController = new MapController();
         map = new Hmap();
         continent = new Continent();
         firstCountry = new Country();
@@ -70,9 +68,9 @@ public class MapControllerTest {
     @Test
     public void removeContinentTest() {
         //run this to add a continent
-        mapController.addContinent(map, continentName, String.valueOf(continentValue), color);
+        MapController.addContinent(map, continentName, String.valueOf(continentValue), color);
 
-        assertTrue(mapController.removeContinent(map, continentName));
+        assertTrue(MapController.removeContinent(map, continentName));
         System.out.println("\"assertTrue\" is passed to test removeContinent method. \n");
     }
 
@@ -82,7 +80,7 @@ public class MapControllerTest {
      */
     @Test
     public void addContinentTest() {
-        assertTrue(mapController.addContinent(map, continentName,
+        assertTrue(MapController.addContinent(map, continentName,
                 String.valueOf(continentValue), color));
         System.out.println("\"assertTrue\" is passed to test addContinent method. \n");
     }
@@ -102,10 +100,10 @@ public class MapControllerTest {
         continentList.add(continent);
         map.setContinents(continentList);
 
-        assertTrue(mapController.containsContinentName(continentList,continentName));
+        assertTrue(MapController.containsContinentName(continentList,continentName));
         System.out.println("\"assertTrue\" is passed to test containsContinentName method. \n");
 
-        assertFalse(mapController.containsContinentName(continentList,"Europe"));
+        assertFalse(MapController.containsContinentName(continentList,"Europe"));
         System.out.println("\"assertFalse\" is passed to test containsContinentName method. \n");
     }
 
@@ -130,7 +128,7 @@ public class MapControllerTest {
         System.out.println("\"assertEquals\" to test for both getName and getValue methods are passed" +
                 " to see the name and value for a continent before applying updateContinent method. \n");
 
-        mapController.updateContinent(continent, map, "New Zealand", "7");
+        MapController.updateContinent(continent, map, "New Zealand", "7");
 
         assertEquals(continent.getValue(), 7);
         assertEquals(continent.getName(), "New Zealand");
@@ -148,10 +146,10 @@ public class MapControllerTest {
         ArrayList<Country> countryList = new ArrayList<>();
         countryList.add(firstCountry);
 
-        assertTrue(mapController.containsCountryName(countryList,firstCountryName));
+        assertTrue(MapController.containsCountryName(countryList,firstCountryName));
         System.out.println("\"assertTrue\" is passed to test containsCountryName method. \n");
 
-        assertFalse(mapController.containsCountryName(countryList,secondCountryName));
+        assertFalse(MapController.containsCountryName(countryList,secondCountryName));
         System.out.println("\"assertFalse\" is passed to test containsCountryName method. \n");
     }
 
@@ -161,7 +159,7 @@ public class MapControllerTest {
      */
     @Test
     public void removeCountryTest() {
-        //declare and run this to add a Country
+        //declare and run these to add a Country
         firstCountry.setName(firstCountryName);
         List<Country> countryList = new ArrayList<>();
         countryList.add(firstCountry);
@@ -170,9 +168,9 @@ public class MapControllerTest {
         List<Continent> continentList = new ArrayList<>();
         continentList.add(continent);
         map.setContinents(continentList);
-        mapController.addCountry(map, firstCountryName,continentName);
+        MapController.addCountry(map, firstCountryName,continentName);
 
-        assertTrue(mapController.removeCountry(map, firstCountryName));
+        assertTrue(MapController.removeCountry(map, firstCountryName));
         System.out.println("\"assertTrue\" is passed to test removeCountry method. \n");
     }
 
@@ -191,10 +189,10 @@ public class MapControllerTest {
         continentList.add(continent);
         map.setContinents(continentList);
 
-        assertFalse(mapController.addCountry(map, firstCountryName, continentName));
+        assertFalse(MapController.addCountry(map, firstCountryName, continentName));
         System.out.println("\"assertFalse\" is passed to test addCountry method. \n");
 
-        assertTrue(mapController.addCountry(map, secondCountryName, continentName));
+        assertTrue(MapController.addCountry(map, secondCountryName, continentName));
         System.out.println("\"assertTrue\" is passed to test addCountry method. \n");
     }
 
@@ -222,11 +220,11 @@ public class MapControllerTest {
         continentList.add(continent);
         map.setContinents(continentList);
 
-        assertEquals(mapController.updateCountry(secondCountry, map,"Germany",
+        assertEquals(MapController.updateCountry(secondCountry, map,"Germany",
                 "3","3", firstCountry), secondCountry);
         System.out.println("\"assertEquals\" is passed to test updateCountry method. \n");
 
-        assertNotEquals(mapController.updateCountry(secondCountry, map,"Germany",
+        assertNotEquals(MapController.updateCountry(secondCountry, map,"Germany",
                 "3","3", firstCountry), firstCountry);
         System.out.println("\"assertNotEquals\" is passed to test updateCountry method. \n");
     }
@@ -237,6 +235,7 @@ public class MapControllerTest {
      */
     @Test
     public void removeNeighborCountryTest() {
+        //first run these to add neighbor country
         firstCountry.setName(firstCountryName);
         secondCountry.setName(secondCountryName);
 
@@ -261,7 +260,7 @@ public class MapControllerTest {
 
         map.setContinents(continentList);
 
-        assertTrue(mapController.removeNeighborCountry(map,firstCountryName,secondCountryName));
+        assertTrue(MapController.removeNeighborCountry(map,firstCountryName,secondCountryName));
         System.out.println("\"assertTrue\" is passed to test removeNeighborCountry method. \n");
     }
 
@@ -292,7 +291,7 @@ public class MapControllerTest {
 
         map.setContinents(continentList);
 
-        assertTrue(mapController.addNeighborCountry(map,firstCountryName,secondCountryName));
+        assertTrue(MapController.addNeighborCountry(map,firstCountryName,secondCountryName));
         System.out.println("\"assertTrue\" is passed to test addNeighborCountry method. \n");
     }
 
@@ -303,7 +302,7 @@ public class MapControllerTest {
     @Test
     public void mapCountryToContinentTest() {
         firstCountry.setName(firstCountryName);
-        assertNotNull(mapController.mapCountryToContinent(new Continent(continentName,continentValue),
+        assertNotNull(MapController.mapCountryToContinent(new Continent(continentName,continentValue),
                 firstCountry));
         System.out.println("\"assertNotNull\" is passed to test mapCountryToContinent method. \n");
     }
